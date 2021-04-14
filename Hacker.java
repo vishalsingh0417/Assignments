895311b01925c601dae23ac74e893b229d4d1dca
Vishal Kumar <82105262+vishalsingh0417@users.noreply.github.com>
2021-04-14 20:04:02 +0530

    Add files via upload

---
+++ Hacker.java
@@ -0,0 +1,9 @@
+package com.assignment.programmer.demo;
+
+public class Hacker extends Programmer{
+	public void display(){
+		System.out.println("Your Code is Hacked");
+		
+	}
+
+}
---
+++ Programmer.java
@@ -0,0 +1,11 @@
+package com.assignment.programmer.demo;
+
+public class Programmer {
+
+	final public void display() {
+		
+		System.out.println("The Original Code");
+		
+	}
+	
+}
